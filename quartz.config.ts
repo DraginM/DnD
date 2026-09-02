ca672cca85cc9507c33a130650456c86d050ad4b
Dragin Maprings <kostya2015ok@yandex.ru>
2026-09-02 18:12:11 +0300

    Quartz sync: Sep 2, 2026, 6:12 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "DnD архив",
+    pageTitleSuffix: "Quartz4 + Osidian",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "ru-RU",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
